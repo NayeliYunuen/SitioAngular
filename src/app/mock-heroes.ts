607e5a93fb9f5cc02ed2375eb25f98d2b354b0d5
NayeliYunuen <201821564@tese.edu.mx>
2021-03-25 23:18:23 -0600

    cambios para agregar los superheroes en lista dinamica

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: "Superman", edad: 30, poder: "Volar, super fuerza", universo: "DC", identidad: "Clark Kent"},
+    {id: 12, name: "Batman", edad: 41, poder: "Habilidades como detective", universo: "DC", identidad: "Bruce Wayne"},
+    {id: 13, name: "Hulk", edad: 35, poder: "Super fuerte", universo: "Marvel", identidad: "Bruce Banner"},
+    {id: 14, name: "Iro Man", edad: 36, poder: "Fuerza sobrehumana y durabilidad", universo: "Marvel", identidad: "Tony Stark"},
+    {id: 15, name: "Thor", edad: 1000, poder: "Dios del trueno", universo: "Marvel", identidad: "Sirgud Jarlson"},
+    {id: 16, name: "Spiderman", edad: 23, poder: "Aracnidos", universo: "Marvel", identidad: "Peter Parker"},
+    {id: 17, name: "Black Widow", edad: 36, poder: "Artes Marciales", universo: "Marvel", identidad: "Natalia Alianovna"},
+    {id: 18, name: "Flash", edad: 28, poder: "Velocidad", universo: "DC", identidad: "Walter West"},
+    {id: 19, name: "Bruja Escarlata", edad: 32, poder: "Proyección astral", universo: "Marvel", identidad: "Wanda Maximoff"},
+    {id: 20, name: "Wonder Woman", edad: 31, poder: "Volar, super fuerza", universo: "DC", identidad: "Diana Prince"},
+]
